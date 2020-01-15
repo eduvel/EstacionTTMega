ebcd54cdf46b6fd6463b80196c29d3cffca8c1e4
eduvel <eduardo.antonio.velazquez@gmail.com>
2020-01-15 13:50:56 -0300

    Division en transmision cada media hora

--- EstacionTTMega.ino
+++ EstacionTTMega.ino
@@ -2,7 +2,7 @@
 #include "manejoSensores.h"
 #include "tramaMQTT.h"
 
-
+int cicloTransmision = 0;
 MQTT moduloGPRS(3, 10);
 void setup() {
 	moduloGPRS.powerOn();
@@ -15,7 +15,11 @@ void loop() {
   if(tiempoMedicionMillis(900)){
     String tramaEnviar =medicionSensores();
     Serial.println(tramaEnviar);
-	moduloGPRS.enviarTramaMQTT(tramaEnviar);
+	cicloTransmision++;
+	if (cicloTransmision >= 2) {
+		moduloGPRS.enviarTramaMQTT(tramaEnviar);
+		cicloTransmision = 0;
+	}
   }
   if (Serial.available() > 0) {
 	  if (Serial.read() == 'L')
